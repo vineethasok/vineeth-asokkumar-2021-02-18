ce8e9f8ea985570d043b1818c7dd3bacd1c07375
VINEETH ASOK <me@vineethasok.com>
2021-02-19 00:30:18 +0530

    Add Search Component

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,29 @@
+import { useState } from 'react'
+import { useRouter } from 'next/router'
+
 import Head from 'next/head'
+import SearchBar from '../components/SearchBar'
 
 const Index = () => {
+  const router = useRouter();
+
+  let [searchValue, setSearchValue] = useState(router.query.keyword || '')
+
+  const updateQuery = (value) => {
+    setSearchValue(value)
+  }
+
   const totalPostings = () => {
     return 1;
   }
+
   return (
     <>
       <Head>
         <title>HEALTH EXPLORE - JOBS</title>
       </Head>
+
+      <SearchBar onKeyEnter={updateQuery} value={searchValue} />
 
       <div className="flex">
         <div className="filters-container">
