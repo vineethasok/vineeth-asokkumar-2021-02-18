1f154df87a3e80a8f6938fce59d6057f330aec86
VINEETH ASOK <me@vineethasok.com>
2021-02-19 00:05:59 +0530

    Add Index Page Outline

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,31 @@
-const Index = () => (
-  <p>
-Hello, Clipboard health!
-  </p>
-)
+import Head from 'next/head'
+
+const Index = () => {
+  const totalPostings = () => {
+    return 1;
+  }
+  return (
+    <>
+      <Head>
+        <title>HEALTH EXPLORE - JOBS</title>
+      </Head>
+
+      <div className="flex">
+        <div className="filters-container">
+          FILTER BOX
+        </div>
+        <div className="company-container">
+          <div className="flex justify-between">
+            <p className="font-bold">{totalPostings()} Job Postings</p>
+            <div className="hidden md:block">
+              SortElements
+            </div>
+
+          </div>
+          CompaniesList
+        </div>
+      </div>
+    </>
+  )
+}
 export default Index
